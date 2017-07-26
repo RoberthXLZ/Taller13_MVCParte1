f221b76383661f1df02f371c96719fc782a00407
user <user@mail.com>
2017-07-26 10:33:38 -0500

    modelo de base creado

--- cliente/models.py
+++ cliente/models.py
@@ -4,3 +4,35 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Factura (models.Model):
+	numero_factura = models.CharField(max_length=16)
+	AGUA = 'Agua'	
+	LUZ = 'Luz'
+	TELEFONO = 'Telefono'
+	FACTURA_OPCIONES = (
+		(AGUA, 'Agua'),
+		(LUZ, 'Luz'),
+		(TELEFONO, 'Telefono'),
+	)
+	nombre_factura = models.CharField(
+		max_length=8,
+		choices=FACTURA_OPCIONES,
+		default=AGUA,
+	)
+	fecha_maxima = models.DateField()
+	cantidad = models.DecimalField(max_digits = 8, decimal_places = 2)
+	PAGADO = 'Pagado'	
+	PENDIENTE = 'Pendiente'
+	ACUMULADO = 'Acumulado'
+	ESTADO_OPCIONES = (
+		(PAGADO, 'Pagado'),
+		(PENDIENTE, 'Pendiente'),
+		(ACUMULADO, 'Acumulado'),
+	)
+	estado_factura = models.CharField(
+		max_length=11,
+		choices=ESTADO_OPCIONES,
+		default=PAGADO,
+	)
+	
