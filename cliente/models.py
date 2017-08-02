8f713e18f8e77e11d0f9300da4e6a381f894f2f7
Alex Ferrin <alex-710@hotmail.com>
2017-08-02 09:05:00 -0500

    Agregado el modelo Recibo

--- cliente/models.py
+++ cliente/models.py
@@ -14,7 +14,7 @@ class Factura (models.Model):
 		(LUZ, 'Luz'),
 		(TELEFONO, 'Telefono'),
 	)
-	nombre_factura = models.CharField(max_length=8,choices=FACTURA_OPCIONES,default=AGUA,)
+	nombre_factura = models.CharField(max_length=8,choices=FACTURA_OPCIONES,default=AGUA)
 	fecha_maxima = models.DateField()
 	cantidad = models.DecimalField(max_digits = 8, decimal_places = 2)
 	PAGADO = 'Pagado'	
@@ -27,3 +27,18 @@ class Factura (models.Model):
 	)
 	estado_factura = models.CharField(max_length=11,choices=ESTADO_OPCIONES,default='Pagado')
 	
+
+class Recibo (models.Model):
+        numero_recibo = models.CharField(max_length=16)
+        echa_pago = models.DateField()
+        nombre_de_quien_recibe = models.CharField(max_length=30)
+        AGUA = 'Agua'
+        LUZ = 'Luz'
+        TELEFONO = 'Telefono'
+        FACTURA_OPCIONES = (
+                (AGUA, 'Agua'),
+                (LUZ, 'Luz'),
+                (TELEFONO, 'Telefono'),
+        )
+        concepto_recibo = models.CharField(max_length=8,choices=FACTURA_OPCIONES,default=AGUA)
+        cantidad = models.DecimalField(max_digits = 8, decimal_places = 2)
